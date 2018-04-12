6b732454e1456ac4c10e4a753b1c04b70a010f18
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2018-04-12 11:39:55 +0200

    Oefening1.pl

---
+++ oefening1.pl
@@ -0,0 +1,17 @@
+# Initialiseer twee twee-dimensionale array's, respectievelijk met 2x3 en met 3x2 elementen. 
+# Interpreteer deze twee-dimensionale array's als matrices, voer de matrix-vermenigvuldiging uit, en schrijf het resultaat uit. 
+    my $x = [
+           [ 3, 2, 3 ],
+           [ 5, 9, 8 ],
+         ];
+    
+    my $y = [
+           [ 4, 7 ],
+           [ 9, 3 ],
+           [ 8, 1 ],
+    ];
+
+    my $aantalCols = $x[0];
+    print $aantalCols;
+
+
