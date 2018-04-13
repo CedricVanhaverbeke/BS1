dfb818d61b420527477f1c5b06ffaec5b4a3affc
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2018-04-13 11:02:56 +0200

    Oefening9

---
+++ reeks3/oefening9.pl
@@ -0,0 +1,27 @@
+# Stel een nieuwe hash samen op basis van meerdere bestaande hashes. Hou, in tegenstelling tot reeks 2 vraag 37, 
+# nu wel rekening met indices die in meerdere originele hashes voorkomen. Probeer ook het herhalen van de while-lus, 
+# voor elk van de hashes, van de oorspronkelijke oplossing te vermijden.
+
+# Strategie: Steek alles in refs
+
+%eerste = ( "Cedric"    =>  "Vanhaverbeke",
+            "Joris"     =>  "Moireau",
+            "Simon"     =>  "De Gheselle"
+                                    );
+%tweede = ( "Cedric"    =>  "Pollie",
+            "Joris"     =>  "Janssens",
+            "Simon"     =>  "De Bruycker"
+                                    );
+
+foreach $ref (\%eerste, \%tweede){
+    while(($key, $value) = each %$ref){
+        push @{$output{$key}}, $value;
+    }
+}
+
+# Printen
+while(($key, $value) = each %output){
+    print $key . ": ";
+    print join ',', @{$value};
+    print "\n";
+}
